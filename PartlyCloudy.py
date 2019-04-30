c63d91ef7125d815e2434f2fd7f4bd838eaae6ad
csPython <cspython@AD25TF04NGF1J-42.local>
2019-04-30 11:57:05 -0400

    clouds added

--- PartlyCloudy.py
+++ PartlyCloudy.py
@@ -1,6 +1,18 @@
 from random import *
 from graphics import *
 
+def draw_circle(X, Y, rad, col, window):
+    circle = Circle(Point(X,Y), rad)
+    circle.setFill(col)
+    circle.setOutline(col)
+    circle.draw(window)
+    
+def draw_clouds(X, Y, rad, col, window):
+    cloud = Circle(Point(X, Y), rad)
+    cloud.setFill(col)
+    cloud.setOutline(col)
+    cloud.draw(window)
+
 window = GraphWin("Partly Cloudy", 1000, 800)
 window.setCoords(0, 0, 1000, 800)
 window.setBackground(color_rgb(134, 206, 250))
@@ -16,3 +28,10 @@ hill = Oval(Point(100, 330), Point(900,-600))
 hill.setFill(color_rgb(0,55,0))
 hill.setOutline(color_rgb(0, 55, 0))
 hill.draw(window)
+
+for i in range(100):
+    cloudX = randint(0, 1000)
+    cloudY = randint(600, 800)
+    cloudRadius = randint(1, 5)
+    cloudColor = "White"
+    draw_clouds(cloudX, cloudY, cloudRadius, cloudColor, window)

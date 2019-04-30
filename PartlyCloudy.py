f16317c5ba8a7ba271f1e3afede77cfd44f25598
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-04-30 11:51:12 -0400

    Fixing old commit

--- PartlyCloudy.py
+++ PartlyCloudy.py
@@ -1,5 +1,18 @@
 from random import *
+from graphics import *
 
 window = GraphWin("Partly Cloudy", 1000, 800)
 window.setCoords(0, 0, 1000, 800)
 window.setBackground(color_rgb(134, 206, 250))
+
+#grass
+grass = Rectangle(Point(0, 0), Point(1000, 200))
+grass.setFill(color_rgb(0, 55, 0))
+grass.setOutline(color_rgb(0, 55, 0))
+grass.draw(window)
+
+#hill
+hill = Oval(Point(100, 330), Point(900,-600))
+hill.setFill(color_rgb(0,55,0))
+hill.setOutline(color_rgb(0, 55, 0))
+hill.draw(window)
